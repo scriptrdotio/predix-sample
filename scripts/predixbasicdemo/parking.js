7d78e1ea6f44ac61f4e4cd47e7fc39ffcd490528
scriptrdotio <git@apstrata.com>
2016-08-17 17:57:25 +0300

    Simplified to decrease response time

--- scripts/predixbasicdemo/parking.js
+++ scripts/predixbasicdemo/parking.js
@@ -3,7 +3,7 @@
 write=nobody
 execute=anonymous 
   **/ 
- var factory = require("modules/predix/factory");
+  var factory = require("modules/predix/factory");
 
 try {
 
@@ -19,7 +19,7 @@ try {
 
   // To simplify, just get the first 20 parking spots at this location (if any)
   var options = {
-      "page":	0,
+      "page": 0,
       "size": 20
   };
   
@@ -30,7 +30,7 @@ try {
   var report = [];
   for (var i = 0; i < result.locations.length; i++) {
     
-    var parkingSpot  =  result.locations[i];console.log(JSON.stringify(parkingSpot));
+    var parkingSpot  =  result.locations[i];
     report.push(findAvailability(parkingSpot));
   }
   
@@ -49,58 +49,44 @@ function findAvailability(parkingSpot) {
   var assetList = parkingSpot.listParkingAssets();
   var asset = assetList[0];
 
-  // Get first page of Vehicles In and Vehicle Out event since the last 48h for the given asset
+  // Get last page of Vehicles In and Vehicle Out event since the last 24h for the given asset
   var endDate = new Date();
   var endTime = endDate.getTime();
-  var startTime = endTime - (480 * 3600000);
+  var startTime = endTime - (24 * 3600000);
   
   var options = {    
-    "size":20,
-    "page":0
+    "size":1,
+    "page":2000000
   };
   
-  var vIn = asset.listVehiculesIn(startTime, endTime, options);console.log("vIn - p1 " + JSON.stringify(vIn));
-  var vOut = asset.listVehiculesOut(startTime, endTime, options);console.log("vOut - p1 " + JSON.stringify(vOut));
-  
-  var totalPagesIn = vIn ? vIn.page.totalPages : 1;
-  var totalPagesOut = vOut ? vOut.page.totalPages : 1;
-  
-  // get last page of In and Out events, since events are sorted in ascending order
-  var optionsIn = {    
-    "size":20,
-    "page":totalPagesIn - 1
-  };
-  
-  var optionsOut = {    
-    "size":20,
-    "page":totalPagesOut - 1
-  };
-  
-  vIn = asset.listVehiculesIn(startTime, endTime, optionsIn);console.log("vIn - p2 " + JSON.stringify(vIn));
-  vOut = asset.listVehiculesOut(startTime, endTime, optionsOut);console.log("vOut - p2 " + JSON.stringify(vOut));
+  var vIn = asset.listVehiculesIn(startTime, endTime, options);
+  var vOut = asset.listVehiculesOut(startTime, endTime, options);
   
   // Get last know In event for the selected parking parking slot
   var lastEventIn = lastEvent(vIn, parkingSpot["location-uid"]);
   
   // get last know Out event for current parking
   var lastEventOut = lastEvent(vOut, parkingSpot["location-uid"]);
-  
+  var lastIn = lastEventIn ? new Date(lastEventIn.timestamp) : new Date("1970-01-01");// no event, we consider parking is available
+  var lastOut = lastEventOut ? new Date(lastEventOut.timestamp) : new Date(); // no event, we consider parking is available
   return {
     
     spot: parkingSpot["location-uid"],
     location: parkingSpot.coordinates,
-    lastIn: new Date(lastEventIn.timestamp), 
-    lastOut: new Date(lastEventOut.timestamp),
-    available: lastEventOut.timestamp > lastEventIn.timestamp
+    lastIn: lastIn, 
+    lastOut: lastOut,
+    available: lastOut.getTime() > lastIn.getTime()
   };
 }
 
 function lastEvent(eventList, parkingId) {
   
   var lastEvent = null;
-  for (var i = 0; eventList && i < eventList._embedded.events.length; i++) {
-    lastEvent = eventList._embedded.events[i]["location-uid"] == parkingId ? eventList._embedded.events[i] : lastEvent;
+  if (eventList) {
+    for (var i =  eventList._embedded.events.length - 1; eventList && lastEvent == null && i >= 0; i--) {
+  	  lastEvent = eventList._embedded.events[i]["location-uid"] == parkingId ? eventList._embedded.events[i] : lastEvent;
+ 	}
   }
   
-  return lastEvent ? lastEvent : {};
+  return lastEvent;
 }			
